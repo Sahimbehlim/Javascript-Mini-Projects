d5d531e6ce1fbfe17fe539bb3682bc702b9f8ab1
Behlim Shaeem <behlimsahim@gmail.com>
2024-10-09 14:31:49 +0530

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,31 @@
-const searchBar = document.querySelector('.header input');
-const searchBtn = document.querySelector('.search-icon-box');
-const weatherBody = document.querySelector('.body');
-const weatherFooter = document.querySelector('.footer');
+require("dotenv").config();
 
-searchBar.focus()
+const searchBar = document.querySelector(".header input");
+const searchBtn = document.querySelector(".search-icon-box");
+const weatherBody = document.querySelector(".body");
+const weatherFooter = document.querySelector(".footer");
+
+searchBar.focus();
+
+console.log(process.env.WEATHER_API_KEY);
 
 // Checking weather functionality
 const checkWeather = async (city) => {
-    // Fetching data
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c56c19f1c7c38425e05537cb48d74e41&units=metric`);
+  // Fetching data
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c56c19f1c7c38425e05537cb48d74e41&units=metric`
+  );
 
-    if (response.status == 404) {
-        alert("Invalid City Name!");
-    } else {
-        let data = await response.json();
+  if (response.status == 404) {
+    alert("Invalid City Name!");
+  } else {
+    let data = await response.json();
 
-        const weatherState = data.weather[0].main.toLowerCase();
+    const weatherState = data.weather[0].main.toLowerCase();
 
-        // Updating weather body as per city
-        weatherBody.classList.add('my-4');
-        weatherBody.innerHTML = `
+    // Updating weather body as per city
+    weatherBody.classList.add("my-4");
+    weatherBody.innerHTML = `
                 <img src="images/${weatherState}.png" alt="">
                 <div class="pt-3">
                     <h1 class="temp">${Math.round(data.main.temp)}°c</h1>
@@ -27,9 +33,9 @@ const checkWeather = async (city) => {
                 </div>
         `;
 
-        // Updating weather footer as per city
-        weatherFooter.classList.add('pt-3', 'pt-sm-4');
-        weatherFooter.innerHTML = `
+    // Updating weather footer as per city
+    weatherFooter.classList.add("pt-3", "pt-sm-4");
+    weatherFooter.innerHTML = `
             <div class="d-flex align-items-center">
                 <img src="images/humidity.png" alt="">
                 <div class="ps-2">
@@ -45,16 +51,16 @@ const checkWeather = async (city) => {
                 </div>
             </div>
         `;
-    }
-    searchBar.value = "";
-}
+  }
+  searchBar.value = "";
+};
 
 // Search for weather info on button click
-searchBtn.addEventListener('click', () => {
-    if (searchBar.value == "") {
-        alert("Input Can't Be Empty!");
-    } else {
-        checkWeather(searchBar.value);
-    }
-    searchBar.focus();
+searchBtn.addEventListener("click", () => {
+  if (searchBar.value == "") {
+    alert("Input Can't Be Empty!");
+  } else {
+    checkWeather(searchBar.value);
+  }
+  searchBar.focus();
 });
