20a0261f9e22a3ea87b2f6528579a260fff68745
Behlim Shaeem <behlimsahim@gmail.com>
2024-10-09 11:43:50 +0530

    chnages made

--- script.js
+++ script.js
@@ -7,20 +7,14 @@ const sortedProjects = projects.sort((a, b) => {
 const projectHTML = sortedProjects
   .map(
     (project, index) =>
-      `<div id="${
-        index + 1
-      }" class="border flex flex-col items-center justify-center gap-1 border-white p-4 rounded-md w-100">
-    <h2 class="text-5xl font-bold">${index < 9 ? "0" : ""}${index + 1}</h2>
-    <div class="flex items-center gap-3">
-      <h2 class="text-3xl w-max">${project.name}</h2>
-      <a
-        href="${project.link}"
-        class="text-2xl border border-white h-8 w-8 rounded-full flex items-center justify-center hover:bg-white hover:text-black transition-all"
-      >
-        <i class="ri-arrow-right-up-line"></i>
-      </a>
+      `
+    <a id="${index + 1}" href="${project.link}" class="text-decoration-none">
+    <div class="border flex flex-col items-center justify-center gap-1 border-white p-4 rounded-md w-full h-full">
+      <h2 class="text-5xl font-bold">${index < 9 ? "0" : ""}${index + 1}</h2>
+      <h2 class="text-3xl text-center">${project.name}</h2>
     </div>
-  </div>`
+    </a>
+  `
   )
   .join("");
 
